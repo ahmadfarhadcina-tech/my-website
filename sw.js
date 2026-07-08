dc8ff7d712a39c5d530020109d2473797f2ab6d1
Farhad Cina <ahmadfarhadcina@gmail.com>
2026-07-08 10:57:43 +0430

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,22 @@
+const cacheName = 'cina-games-v1';
+const assets = [
+  './',
+  './index.html',
+  './profile.jpg'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cachedResponse => {
+      return cachedResponse || fetch(e.request);
+    })
+  );
+});
